88adb4ebe8aa00046c7b59b75ce1a766652666dd
Bas Broek <bas@basbroek.nl>
2016-12-13 17:57:16 +0100

    Add DAO protocol

---
+++ Twitter/DAO.swift
@@ -0,0 +1,16 @@
+//
+//  DAO.swift
+//  Twitter
+//
+//  Created by Bas Broek on 13/12/2016.
+//  Copyright © 2016 Bas Broek. All rights reserved.
+//
+
+import Foundation
+
+protocol DAO {
+  func create()
+  func read()
+  func update()
+  func delete()
+}
