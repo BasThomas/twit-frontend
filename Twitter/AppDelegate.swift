35f0785ac54fa399b591fce162e727e38e3f6adb
Bas Broek <bas@basbroek.nl>
2017-01-17 15:14:09 +0100

    Re-enable GO

--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -16,25 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
-//    Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { _ in
-//      print("Sending queued tweets...")
-//      queued.forEach { tweet in
-//        Tweet.create(object: tweet) { result in
-//          switch result {
-//          case .success:
-//            print("Sent tweet! (\(tweet.content))")
-//          case let .failure(error):
-//            print("Error sending tweet: \(error)")
-//          }
-//        }
-//      }
-//      
-//      if queued.isEmpty {
-//        print("No tweets were queued")
-//      } else {
-//        queued.removeAll()
-//      }
-//    }
+    Timer.scheduledTimer(withTimeInterval: 10.0, repeats: true) { _ in
+      print("Sending queued tweets...")
+      queued.forEach { tweet in
+        Tweet.create(object: tweet) { result in
+          switch result {
+          case .success:
+            print("Sent tweet! (\(tweet.content))")
+          case let .failure(error):
+            print("Error sending tweet: \(error)")
+          }
+        }
+      }
+      
+      if queued.isEmpty {
+        print("No tweets were queued")
+      } else {
+        queued.removeAll()
+      }
+    }
     
     return true
   }
